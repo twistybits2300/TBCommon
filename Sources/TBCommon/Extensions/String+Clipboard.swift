71d3f66c8ad8a98820d15b8af0c3cba7757ec5f4
twistybits2300 <twistybits2300@protonmail.com>
2024-05-16 12:33:17 -0700

    Add utility functions for putting text or URLs onto the clipboard

--- Sources/TBCommon/Extensions/String+Clipboard.swift
+++ Sources/TBCommon/Extensions/String+Clipboard.swift
@@ -1,6 +1,8 @@
 import Foundation
 
 public extension String {
+    /// Puts the contents of this string onto 
+    /// the generally available clipboard.
     func putOnPasteboard() {
         Clipboard.general.paste(self)
     }
--- Sources/TBCommon/Extensions/URL+StringLiteral.swift
+++ Sources/TBCommon/Extensions/URL+StringLiteral.swift
@@ -1,13 +1,23 @@
 import Foundation
 
 public extension URL {
+    /// Utility for creating a `URL` given a `text` string.
+    /// - Parameters:
+    ///   - text: The text to be converted into a `URL`
+    ///
+    /// ⚠️ WARNING - USE THIS WITH CAUTION ⚠️
+    ///
+    /// A `preconditionFailure()` will be triggered if the
+    /// conversion of the given `text` into a `URL` fails.
     init(stringLiteral text: String, file: StaticString = #file, line: UInt = #line) {
         guard let url = URL(string: text) else {
             preconditionFailure("URL not created - invalid `stringLiteral`: \(text)", file: file, line: line)
         }
         self = url
     }
-
+    
+    /// Puts the contents of this `URL` onto
+    /// the generally available clipboard.
     func putOnPasteboard() {
         absoluteString.putOnPasteboard()
     }
