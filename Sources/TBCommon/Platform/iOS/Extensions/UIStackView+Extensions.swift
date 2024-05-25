4bc3628bcc96cb0dd90780b282fd8fcc96554ccd
twistybits2300 <twistybits2300@protonmail.com>
2024-05-25 06:13:24 -0700

    Functions for adding background or foreground views to stack views

--- Sources/TBCommon/Platform/iOS/Extensions/UIStackView+Extensions.swift
+++ Sources/TBCommon/Platform/iOS/Extensions/UIStackView+Extensions.swift
@@ -1,6 +1,7 @@
 #if os(iOS)
 import UIKit
 
+@available(iOS 13.0, *)
 public extension UIStackView {
     /// Creates a horizontal stack using the given subviews
     /// - Parameters:
@@ -42,6 +43,55 @@ public extension UIStackView {
         return stack
     }
     
+    /// Adds a new view of the given `color` and `radius` to the background
+    /// of this stack view.
+    /// - Parameters:
+    ///   - color: The new view's background color.
+    ///   - radius: The new view's corner radius.
+    /// - Returns: The newly created view. Can be discarded.
+    @discardableResult
+    func addBackgroundView(color: UIColor, radius: CGFloat = 0) -> UIView {
+        addChildView(color: color, radius: radius)
+    }
+    
+    /// Adds a new view of the given `color` and `radius` to the foreground
+    /// of this stack view.
+    /// - Parameters:
+    ///   - color: The new view's background color.
+    ///   - radius: The new view's corner radius.
+    /// - Returns: The newly created view. Can be discarded.
+    @discardableResult
+    func addForegroundView(color: UIColor, radius: CGFloat = 0) -> UIView {
+        let index = subviews.count
+        return addChildView(color: color, radius: radius, at: index)
+    }
+
+    /// Adds a new view whose background color is set to `color`,
+    /// corner radius is set to `radius` at the given subview `index`.
+    /// - Parameters:
+    ///   - color: The new view's background color.
+    ///   - radius: The new view's corner radius.
+    ///   - index: The subview index to insert the new view at.
+    /// - Returns: A new view. Can be discarded.
+    @discardableResult
+    func addChildView(color: UIColor,
+                      radius: CGFloat = 0,
+                      at index: Int = 0) -> UIView {
+        let view = UIView()
+        view.useAutoLayout()
+        view.backgroundColor = color
+        view.layer.cornerRadius = radius
+        insertSubview(view, at: index)
+        NSLayoutConstraint.activate([
+            view.topAnchor.constraint(equalTo: topAnchor),
+            view.leadingAnchor.constraint(equalTo: leadingAnchor),
+            view.bottomAnchor.constraint(equalTo: bottomAnchor),
+            view.trailingAnchor.constraint(equalTo: trailingAnchor),
+        ])
+        return view
+    }
+
+    // MARK: - Utilities
     private static func makeStack(subviews: [UIView],
                            distribution: UIStackView.Distribution = .fill,
                            alignment: UIStackView.Alignment = .fill,
--- Tests/TBCommonTests/Platform/iOS/Exdtensions/UIStackView+ExtensionsTests.swift
+++ Tests/TBCommonTests/Platform/iOS/Exdtensions/UIStackView+ExtensionsTests.swift
@@ -34,6 +34,64 @@ final class UIStackView_ExtensionsTests: XCTestCase {
         XCTAssertEqual(sut.alignment, fixture.alignment)
         XCTAssertEqual(sut.spacing, fixture.spacing)
     }
+    
+    /// Validates that `addBackgroundView()` works as expected.
+    func test_addBackgroundView() throws {
+        let subviews = fixture.subviews
+        let sut = UIStackView.verticalStack(subviews: subviews)
+        XCTAssertEqual(sut.subviews.count, 2)
+
+        let backgroundView = sut.addBackgroundView(color: .red)
+        
+        let firstView = try XCTUnwrap(sut.subviews.first)
+        XCTAssertEqual(firstView, backgroundView)
+    }
+    
+    /// Validates that `addForegroundView()` works as expected.
+    func test_addForegroundView() throws {
+        let subviews = fixture.subviews
+        let expectedIndex = subviews.count
+
+        let sut = UIStackView.verticalStack(subviews: subviews)
+        XCTAssertEqual(sut.subviews.count, 2)
+
+        let foregroundView = sut.addForegroundView(color: .red)
+        XCTAssertEqual(sut.subviews.count, 3)
+
+        let returnedView = sut.subviews[expectedIndex]
+        XCTAssertEqual(returnedView, foregroundView)
+    }
+    
+    /// Validates that `addChildView(color:radius:at:)` works as expected.
+    func test_addChild_default() throws {
+        let subviews = fixture.subviews
+        let sut = UIStackView.verticalStack(subviews: subviews)
+        XCTAssertEqual(sut.subviews.count, 2)
+        
+        
+        let addedView = sut.addChildView(color: .black)
+        XCTAssertEqual(sut.subviews.count, 3)
+        XCTAssertEqual(addedView.layer.cornerRadius, 0)
+        
+        let firstView = try XCTUnwrap(sut.subviews.first)
+        XCTAssertEqual(firstView, addedView)
+    }
+    
+    /// Validates that `addChildView(color:radius:at:)` properly inserts
+    /// the new view at the indicated index.
+    func test_addChild() throws {
+        let subviews = fixture.subviews
+        let expectedIndex = subviews.count - 1
+
+        let sut = UIStackView.verticalStack(subviews: subviews)
+        XCTAssertEqual(sut.subviews.count, 2)
+
+        let addedView = sut.addChildView(color: .blue, at: expectedIndex)
+        XCTAssertEqual(sut.subviews.count, 3)
+        
+        let returnedView = sut.subviews[expectedIndex]
+        XCTAssertEqual(returnedView, addedView)
+    }
 }
 
 private struct TestFixture {
